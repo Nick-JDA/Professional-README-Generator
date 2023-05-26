7629ef6d80254b56a534ce0167d3b3efd0e0b6af
Nick JDA <armstrongnick1999@gmail.com>
2023-05-26 19:37:32 -0400

    Added switch statements to badges and license functions in generateMarkdown.js, moved badges section to top of pre-rendered readme file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no license, return an empty string 
+
+function renderLicenseBadge(license) {
+  let badgers = '';
+
+  switch (license) {
+    case 'nothing':
+      return badgers;
+
+    case 'Unlicensed':
+      return badgers += '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+
+    case 'GNU AGPLv3':
+      return badgers += '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+
+    case 'GNU GPLv3':
+      return badgers += '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+
+    case 'GNU LGPLv3':
+      return badgers += '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+
+    case 'Mozilla Public License 2.0':
+      return badgers += '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+
+    case 'Apache License 2.0':
+      return badgers += '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+
+    case 'MIT License':
+      return badgers += '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+    case 'Boost Software License 1.0':
+      return badgers += '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+      
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let lyesenseLink = '';
+
+  switch (license) {
+    case 'nothing':
+      return lyesenseLink;
+
+    case 'Unlicensed':
+      return lyesenseLink += '[License: Unlicense](https://choosealicense.com/licenses/unlicense/)';
+
+    case 'GNU AGPLv3':
+      return lyesenseLink += '[License: AGPL v3](https://choosealicense.com/licenses/agpl-3.0/)';
+
+    case 'GNU GPLv3':
+      return lyesenseLink += '[License: GPL v3](https://choosealicense.com/licenses/gpl-3.0/)';
+
+    case 'GNU LGPLv3':
+      return lyesenseLink += '[License: LGPL v3](https://choosealicense.com/licenses/lgpl-3.0/)';
+
+    case 'Mozilla Public License 2.0':
+      return lyesenseLink += '[License: MPL 2.0](https://choosealicense.com/licenses/mpl-2.0/)';
+
+    case 'Apache License 2.0':
+      return lyesenseLink += '[License: Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)';
+
+    case 'MIT License':
+      return lyesenseLink += '[License: MIT](https://choosealicense.com/licenses/mit/)';
+
+    case 'Boost Software License 1.0':
+      return lyesenseLink += '[License: Boost Software 1.0](https://choosealicense.com/licenses/bsl-1.0/)';
+      
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,7 +84,7 @@ function generateMarkdown(data) {
 
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+${data.badges}
 
 ## Description
 
